da984094ca7d4ffd0cbf10b6c08e44780725e297
thukhaedu-glitch <thukha.edu@gmail.com>
2026-05-31 05:32:00 +0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  server: { open: true },
-  plugins: [reactRefresh()]
-});
+  plugins: [react()],
+})
