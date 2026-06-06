b3d62954c245dd158dab7eb3fcd4403b229c7890
thukhaedu-glitch <thukha.edu@gmail.com>
2026-06-06 15:34:30 +0400

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import react from'@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+})
+
+
+
+
+export default defineConfig({
+plugins:[react()],
+build:{
+rollupOptions:{
+external:[],
+},
+},
+optimizeDeps:{
+include:['jsqr'],
+},
 })
