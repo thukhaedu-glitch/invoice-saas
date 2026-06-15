269e16c5bd935691acff47ccb7efe2045e0433f7
thukhaedu-glitch <thukha.edu@gmail.com>
2026-06-15 05:01:13 +0400

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import{useState,useEffect}from'react'
+import React,{useState,useEffect}from'react'
 import{db,auth,storage}from'../firebase'
 import{doc,getDoc,setDoc,getDocs,collection,query,where,updateDoc}from'firebase/firestore'
 import{updatePassword,reauthenticateWithCredential,EmailAuthProvider}from'firebase/auth'
